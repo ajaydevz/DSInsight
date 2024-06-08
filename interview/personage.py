64716eda2c1d3bfe884e8a1f17da45a5894e75b2
Ajay dev <ajaydevzzz30@gmail.com>
2024-06-08 19:17:22 +0530

    update

--- interview/personage.py
+++ interview/personage.py
@@ -16,3 +16,18 @@ print(p.calculate_age())
 
 
 
+from datetime import datetime
+
+class Person:
+    def __init__(self,name,date_of_birth,location):
+        self.name = name
+        self.date_of_birth = date_of_birth
+        self.location = location
+
+    def calculate_age(self):
+        today = datetime.today()
+        age = today.year - self.date_of_birth - ((today.month - today.day) < (self.date_of_birth.month - self.date_of_birth.day))
+        return age
+    
+p = Person("john",datetime(1994,5,15),"new york")
+
