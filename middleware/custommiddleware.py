73c1b82c9d46ea8adf2af68fda60d58975c815eb
Ajay dev <ajaydevzzz30@gmail.com>
2024-06-10 01:54:05 +0530

    custom-middleware

--- middleware/custommiddleware.py
+++ middleware/custommiddleware.py
@@ -1,14 +1,10 @@
-from django.shortcuts import redirect
-from django.urls import reverse
-
-class ExampleMiddleware:
+class CustomMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
+        # Code to execute for each request before the view (and later middleware) is called.
+        
         response = self.get_response(request)
-        if request.path == reverse('some-view'):
-            return redirect('another-view')
+        # Code to execute for each response after the view (and earlier middleware) is called.
         return response
-    
-    
